f96e13d0d82fce35ddb52d59862fe0bb225c02d4
黄文斌 <huangwb@yealink.com>
2021-04-22 08:46:33 +0800

    增加大小固定有序数组的实现

--- 数组-基础/大小固定的有序数组.js
+++ 数组-基础/大小固定的有序数组.js
@@ -1,23 +1,38 @@
 class SortedArray {
     constructor(length) {
       this.length = length;
-      this.arr = new Array(length);
+      this.arr = new Array();
     }
     insert(item) {
       if (this.arr.length == 0) {
           this.arr.push(item);
+          return;
       }
       let i = this.arr.length - 1;
       for (i; i >= 0; i--) {
           if (this.arr[i] <= item) {
               break;
           } else {
-              a[i+1] = a[i];
+              this.arr[i+1] = this.arr[i];
           }
       }
-      a[i+1] = item;
+      this.arr[i+1] = item;
       if (this.arr.length > this.length) {
           this.arr = this.arr.slice(this.arr.length - this.length);
+      }
+    }
+    delete(item) {
+      let index = this.arr.indexOf(item);
+      if (index > -1) {
+        this.arr.splice(index, 1);
+        return true;
+      }
+      console.warn(`item ${item} does not exists`);
+      return false;
+    }
+    update(oldItem, newItem) {
+      if (this.delete(oldItem)) {
+        this.insert(newItem)
       }
     }
 }
